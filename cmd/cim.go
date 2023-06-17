46438b342c091f7713435e07002285407ebe0ebf
Mikael Vandmo <mikael@vandmo.se>
2023-06-17 10:48:34 +0000

    X

--- cmd/cim.go
+++ cmd/cim.go
@@ -1,19 +1,19 @@
 package cmd
 
 import (
-    "encoding/json"
+	"encoding/json"
 	"fmt"
-    "io/ioutil"
-	
+	"os"
+
+	"github.com/fxamacker/cbor/v2"
 	"github.com/martinlindhe/base36"
 	"github.com/spf13/cobra"
-	"github.com/fxamacker/cbor/v2"
 )
 
 type Data struct {
-    Name string `json:"name"`
-    Url string `json:"url"`
-    Key []byte `json:"public-key"`
+	Name string `json:"name"`
+	Url  string `json:"url"`
+	Key  []byte `json:"public-key"`
 }
 
 var cimCmd = &cobra.Command{
@@ -22,7 +22,7 @@ var cimCmd = &cobra.Command{
 	Args:  cobra.MatchAll(cobra.ExactArgs(1), cobra.OnlyValidArgs),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		filename := args[0]
-		byteValue, _ := ioutil.ReadFile(filename)
+		byteValue, _ := os.ReadFile(filename)
 		var data Data
 		json.Unmarshal(byteValue, &data)
 		b, _ := cbor.Marshal(data)
